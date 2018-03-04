9cf59940701e8d34d1c4cc7ff4b8e163e5cab0a0
Alistair Laing <aliuk2012@users.noreply.github.com>
2018-03-04 20:44:07 +0000

    Basic calculations and styling

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -6,5 +6,36 @@ import {Component} from '@angular/core';
 })
 
 export class CalculatorComponent {
+  numberClicked: number = 0;
+  operationClicked: string = "+";
+  totalValue: number = 0;
 
+  getNumberClicked() {
+    let value = this.numberClicked;
+    value = value/100;
+    return "£"+((Number(value)||0).toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,"));
+  }
+
+  getTotalValue() {
+    let value = this.totalValue;
+    value = value/100;
+    return "£"+((Number(value)||0).toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,"));
+  }
+
+  onClickedNumber(event: any) {
+    if(this.numberClicked === 0){
+      this.numberClicked = event.target.value;
+    } else {
+      this.numberClicked += event.target.value;
+    }
+  }
+
+  onClickedOperator(event: any) {
+    if (event.target.value === "+") {
+      this.totalValue = Number(this.totalValue) + Number(this.numberClicked);
+    } else {
+      this.totalValue = Number(this.totalValue) - Number(this.numberClicked);
+    }
+    this.numberClicked = 0;
+  }
 }
